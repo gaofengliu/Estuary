8c1858374f3b20e01419f4073070bfd7d0d8f1fe
gaofengliu <iphone10s@163.com>
2017-07-16 20:40:54 +0800

    draw some coastlines map

---
+++ yjg/cjkmap.py
@@ -0,0 +1,49 @@
+__author__ = 'mac'
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+import matplotlib
+import matplotlib.pylab as plt
+from matplotlib.pylab import rcParams
+
+rcParams['figure.figsize']= 10,5
+font = {'family': 'normal','weight':'bold','size':10}
+
+plt.rcParams['font.sans-serif']=['SimHei']
+
+matplotlib.rc('font',**font)
+
+def draw_cjk_map():
+#    with open('yjg_coastline.txt') as f:
+#         coastlines = f.readlines()
+    yjgcoast = np.loadtxt('yjg_coastline.txt')
+    hbykmt = np.loadtxt('hbykmt.txt')
+    hjmt = np.loadtxt('hjmt.txt')
+    lycmt = np.loadtxt('lycmt.txt')
+    shmt = np.loadtxt('shmt.txt')
+
+    fig,ax=plt.subplots()    
+    
+    ax.plot(yjgcoast[:,0],yjgcoast[:,1]) 
+    ax.plot(hbykmt[:,0],hbykmt[:,1])
+    ax.plot(hjmt[:,0],hjmt[:,1]) 
+    ax.plot(lycmt[:,0],lycmt[:,1])
+    ax.plot(shmt[:,0],shmt[:,1]) 
+    
+
+
+#    startline = 0
+#    for ii in range(startline,startline+175):
+#        data[ii] = coastlines[ii].split()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    draw_cjk_map()
+
+
